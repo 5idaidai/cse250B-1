40ec5cf95b475f0261076c4e913ef71402731f66
Alex <axydes@gmail.com>
2014-01-14 17:47:42 -0800

    First draft of software framework
    Adding a couple of files to get us started.

---
+++ lbfgs.m
@@ -0,0 +1,9 @@
+function [] = lbfgs()
+%LBFGS Run L-BFGS on the dataset
+%   Run Stochastic gradient descent with L2 regularization
+%       on the dataset. Use grid search to find values for
+%       the hyperparameters
+
+
+end
+
