fixed overflow/underflow errors with log sum exp trick

project1/python/logistic_regression.py
@@ -1,43 +1,31 @@
 from __future__ import division
 
-import collections
-
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 
 from sklearn.utils.random import check_random_state
 
 
-def _sigmoid(z):
-    if z > 36:
-        return 1 - 1e-9
-    elif z < -709:
-        return 1e-9
-    else:
-        return 1 / (1 + np.exp(-z))
+def log_sum_exp(x):
+    m = x.max()
+    x = x - m
+    return m + np.log(np.exp(x).sum())
 
 
-def sigmoid(z):
-    if isinstance(z, collections.Iterable):
-        return np.array(list(_sigmoid(n) for n in np.nditer(z)))
-    else:
-        return _sigmoid(z)
+def log_prob(y, xb):
+    """returns log prob(Y=y)
 
+    xb = np.dot(X, betas)
 
-def prob(y, x, betas):
-    """prob(Y=y | X=x, betas)"""
-    p = sigmoid((x * betas).sum())
-    if y == 0:
-        p = 1 - p
-    if p == 0:  # TODO: this is a hack
-        raise Exception('this should never happen')
-        p = 1e-9
-    return p
+    """
+    if y == 1:
+        xb = -xb
+    return -log_sum_exp(np.array([0, xb]))
 
 
 def lcl(data, labels, betas):
     """log conditional likelihood"""
-    return sum(np.log(prob(y, x, betas)) for x, y in zip(data, labels))
+    return sum(log_prob(y, np.dot(x, betas)) for x, y in zip(data, labels))
 
 
 def rlcl(data, labels, betas, mu):
@@ -48,7 +36,7 @@ def rlcl(data, labels, betas, mu):
 
 def lcl_prime(data, labels, betas):
     """gradient of lcl"""
-    coeffs = np.array(list(y - prob(1, x, betas)
+    coeffs = np.array(list(y - np.exp(log_prob(1, np.dot(x, betas)))
                            for x, y in zip(data, labels)))
     return coeffs.dot(data)
 
@@ -140,8 +128,10 @@ class LogisticRegression(object):
         self._validate_args()
         X = self._preprocess_data(X)
         X = self._normalize_data(X)
-        z = self.intercept_ + np.dot(X, self.coefficients_)
-        probs = sigmoid(z)
+        XB = self.intercept_ + np.dot(X, self.coefficients_)
+        XB = XB.reshape(-1, 1)
+        f = lambda z: log_prob(1, z)
+        probs = np.exp(np.apply_along_axis(f, axis=1, arr=XB))
         neg, pos = self.old_labels_
         return np.where(probs.ravel() >= 0.5, pos, neg)
 
@@ -173,7 +163,7 @@ class LogisticRegression(object):
 
     def _sgd_update(self, betas, x, y, lambda_):
         """single step in SGD"""
-        p = prob(1, x, betas)
+        p = np.exp(log_prob(1, np.dot(x, betas)))
         result = betas + lambda_ * ((y - p) * x - 2 * self.mu * betas)
         # do not regularize intercept
         result[0] = betas[0] + lambda_ * ((y - p) * x[0])

project1/python/test_logistic_regression.py
@@ -22,7 +22,7 @@ class TestPrimes(unittest.TestCase):
         self.data = lr.LogisticRegression._preprocess_data(data)
         self.labels, _ = lr.LogisticRegression._preprocess_labels(labels)
         state = np.random.RandomState(0)
-        self.betas = state.uniform(-1, 1, size=(n_betas, data.shape[1]))
+        self.betas = state.uniform(-10, 10, size=(n_betas, data.shape[1]))
 
     def _test(self, f, fprime):
         for b in self.betas:
@@ -42,17 +42,6 @@ class TestPrimes(unittest.TestCase):
 
 class TestLogisticRegression(unittest.TestCase):
 
-    def test_sigmoid(self):
-        data = np.array(range(-50000, 0, 10))
-        data2 = np.array(range(0, 50000, 10))
-
-        negs = lr.sigmoid(data)
-        pos = lr.sigmoid(data2)
-
-        assert max(negs) <= 0.5
-        assert min(negs) > 0
-        assert min(pos) >= 0.5
-        assert max(pos) < 1
 
     def test_preprocess_labels(self):
         labels = [-1, 1, -1]
@@ -68,19 +57,6 @@ class TestLogisticRegression(unittest.TestCase):
         model.fit(data, labels)
         predictions = model.predict(data)
         self.assertTrue(np.all(predictions == labels))
-
-    def test_rlcl(self):
-        data = [[40000, 0, 1],
-                [1, 1, -40000]]
-        labels = [0, 1]
-        betas = np.array([1, 1, 1])
-        mu = 2
-        result = lr.rlcl(data, labels, betas, mu)
-        s1 = lr.sigmoid(40001)
-        s2 = lr.sigmoid(-39998)
-        expected = (np.log(1 - s1) + np.log(s2) -
-                    mu * 2)
-        self.assertEquals(result, expected)
 
     def test_sgd(self):
         data, labels = make_blobs(n_features=5, centers=2)