be9f87eaa9c3ee94c09fbe7fc1f97982620d1910
Alex <axydes@gmail.com>
2014-02-04 20:29:18 -0800

    Test feature function: last tag is period

--- project2/testGenerateFFs.py
+++ project2/testGenerateFFs.py
@@ -23,8 +23,9 @@ def testffB2(yi1, yi, b):
 def testffA3(x, i, n, a):
     return 1
 
+#test case: last tag is a period
 def testffB3(yi1, yi, b):
-    return yi == tags.tags[7]
+    return yi == tags.tags[1] and yi1 == tags.tags[3]
 
 if __name__ == "__main__":
     ffs = []
@@ -34,20 +35,20 @@ if __name__ == "__main__":
     temp1['bset'] = []
     temp1['A'] = testffA1
     temp1['B'] = testffB1
-    ffs.append(temp1)
+    #ffs.append(temp1)
     
     temp2 = {}
     temp2['aset'] = []
     temp2['bset'] = tags.tags
     temp2['A'] = testffA2
     temp2['B'] = testffB2
-    ffs.append(temp2)
+    #ffs.append(temp2)
     
     temp3 = {}
     temp3['aset'] = []
     temp3['bset'] = []
     temp3['A'] = testffA3
     temp3['B'] = testffB3
-    ffs.append(temp2)
+    ffs.append(temp3)
     
     gFF.generateFFs(ffs, os.path.splitext(os.path.basename(__file__))[0])
