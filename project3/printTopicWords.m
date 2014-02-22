491d5601bc707275f5da83ac1bf7b85236644f0e
Alex Xydes <axydes@gmail.com>
2014-02-21 17:36:13 -0800

    Adding in printing of phi

---
+++ project3/printTopicWords.m
@@ -0,0 +1,19 @@
+function [idx,val] = printTopicWords(phis,vocab,k)
+%UNTITLED3 Print top k words in each topic
+%   Detailed explanation goes here
+
+    for i=1:size(phis,1)
+        topic=phis(i,:);
+        [j,idx,val] = find(topic);
+        [sval,sidx]=sort(val(:),'descend');
+        
+        fprintf('Topic %d\n',i);
+        for j=1:k
+            cell=vocab(sidx(j));
+            fprintf('%s\n', cell{1});
+        end
+        fprintf('\n');
+    end
+
+end
+
--- project3/testscript.m
+++ project3/testscript.m
@@ -1,7 +1,14 @@
 
 load('classic400.mat');
 %load('20Newsgroups.mat');
+
+bag=classic400;
+voc=classicwordlist;
+
 numTopics = 4;
 numEpochs = 30;
     
-[thetas,phis] = lda(numTopics, classic400, classicwordlist, numEpochs);
+[thetas,phis] = lda(numTopics, bag, voc, numEpochs);
+
+plotDocTopics(thetas);
+printTopicWords(phis,voc,numTopics);
