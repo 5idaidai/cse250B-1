718d30391cefb97fc33fabfadf4e6f68d18462bd
Alex Xydes <axydes@gmail.com>
2014-02-22 14:02:26 -0800

    Updating test script

--- project3/testscript.m
+++ project3/testscript.m
@@ -8,9 +8,12 @@ voc=classicwordlist;
 numTopics = 10;
 numEpochs = 30;
 percCutOff = 0.15;
+
+
+words = get_words(bag, voc, sum(sum(bag)), size(bag,1));
     
 tic;
-[thetas,phis] = lda(bag, voc, numTopics, numEpochs, percCutOff);
+[thetas,phis] = lda(bag, voc, words, numTopics, numEpochs, percCutOff);
 TimeSpent = toc;
 
 fprintf('LDA took %f seconds (aka %f minutes).\n\n',TimeSpent,TimeSpent/60);
