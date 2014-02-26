745fd2db05f70c2f946fe284489335b7eab863dd
Alex Xydes <axydes@gmail.com>
2014-02-25 18:15:26 -0800

    Changes to my gridsearch script

--- project3/gridsearch.m
+++ project3/gridsearch.m
@@ -7,11 +7,11 @@ voc=classicwordlist;
 fprintf('Running grid search on: %s\n',file);
 
 numEpochs = 100;
-numTopics = [2];%[2,3,4,5,10];
+numTopics = [2,3,4,5,10];
 percs = [0.1];%[0.02,0.1,0.15,0.2];
 
-beta = 0.1;
-alpha = 5/numTopics;
+storedBetas = 0.01;
+storedAlphas = 50./numTopics;
 
 
 nT = size(numTopics,1);
@@ -20,6 +20,7 @@ times = zeros(nP,nT);
 storedThetas = cell(nP,nT);
 storedPhis = cell(nP,nT);
 storedRatios = cell(nP,nT);
+storedChangedThetas = cell(nP,nT);
 
 words = get_words(bag, voc, sum(sum(bag)), size(bag,1));
 fprintf('\n');
@@ -28,15 +29,18 @@ kidx = 1;
 pidx = 1;
 for percCutOff=percs
     for k=numTopics
+        alpha = storedAlphas(k);
+        beta = storedBetas;
         fprintf('Running LDA with %d topics, %.04f percent cutoff\n',k,percCutOff);
         
         tic;
-        [thetas,phis,ratios,alphas,betas] = lda(bag, voc, words, alpha, beta, k, numEpochs, percCutOff);
+        [thetas,phis,ratios,alphas,betas,changeTheta ] = lda(bag, voc, words, alpha, beta, k, numEpochs, percCutOff);
         times(pidx,kidx) = toc;
         
         storedThetas(pidx,kidx) = {thetas};
         storedPhis(pidx,kidx) = {phis};
         storedRatios(pidx,kidx) = {ratios};
+        storedChangedThetas(pidx,kidx) = {changeTheta};
 
         fprintf('LDA took %f seconds (aka %f minutes).\n\n',times(pidx,kidx),times(pidx,kidx)/60);
         
@@ -50,8 +54,8 @@ plot(temp{1});
 temp = storedPhis(pidx-1,kidx-1);
 printTopKWords(temp{1},voc,10);
 
-resultsFile = strcat(file,'_2topics_100epochs_newalphasbetas_grid_results.mat');
-%save(resultsFile,'times','storedThetas','storedPhis','storedRatios','alpha','beta','voc','numEpochs','numTopics','percs');
+resultsFile = sprintf('%s_%dtopics_%depochs_grid_results.mat',file,numTopics,numEpochs);
+save(resultsFile,'times','storedThetas','storedChangedThetas','storedPhis','storedRatios','storedAlphas','storedBetas','voc','numEpochs','numTopics','percs');
 %end
 
 %alphabeta_search
