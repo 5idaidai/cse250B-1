c38859fcc10fb885834fa434e12c638ff4d5ce73
Alex Xydes <axydes@gmail.com>
2014-03-04 19:44:46 -0800

    Added some more initialization, and an iteration through the sentences

--- project4A/runTrainTest.m
+++ project4A/runTrainTest.m
@@ -1,10 +1,31 @@
 
-load('codeDataMoviesEMNLP/data/rt-polaritydata/vocab.mat');
+if exist('words','var') == 0
+    load('codeDataMoviesEMNLP/data/rt-polaritydata/vocab.mat');
+end
+if exist('allSNum','var') == 0
+    load('codeDataMoviesEMNLP/data/rt-polaritydata/RTData_CV1.mat','allSNum','labels');
+end
 
 %init meaning vectors for each word to random values
 d = 20;
-meanings = rand(d,size(words,2));
+meanings = normrnd(0,1,d,size(words,2));
 
 %init W and b randomly
 W = rand(d,2*d);
 b = rand(d,1);
+
+%init U and c for backpropagation
+U = rand(2*d,d);
+c = rand(2*d,1);
+
+%iterate through all sentences
+for i=1:length(allSNum)
+    %get sentence
+    sent=allSNum(i);
+    sent=sent{1,1};
+    
+    %get meaning vectors for each word
+    sentMean = meanings(:,sent);
+    
+    %greedy tree algorithm
+end
