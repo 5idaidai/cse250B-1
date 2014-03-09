6c74c2ceefda12e114a2ca189f7f4d279ff0a12d
Alex Xydes <axydes@gmail.com>
2014-03-08 19:39:24 -0800

    Changing square loss back to square of euclidean norm

--- project4A/squareLoss.m
+++ project4A/squareLoss.m
@@ -2,7 +2,7 @@ function [ err ] = squareLoss( xi, zi, num, denom )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-    err = (num/denom) * (0.5*((xi - zi)^2));
+    err = (num/denom) * (norm(xi-zi,2)^2);
 
 end
 
