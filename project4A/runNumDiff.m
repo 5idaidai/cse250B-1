df1b6ec7b3dac01ca86ec335ff3c2842131e68aa
Sandi Calhoun <sandi.calhoun@gmail.com>
2014-03-14 22:12:35 -0700

    fixed runNumDiff

--- project4A/runNumDiff.m
+++ project4A/runNumDiff.m
@@ -14,7 +14,7 @@ trainInput = 0;%don't train input for now
 d = 20;
 lambda = [1e-05, 0.0001, 1e-07, 0.01];
 alpha = 0.4;
-maxIter = 70;
+maxIter = 1;
 
 %init meaning vectors for each word to random values
 meanings = normrnd(0,1,d,size(words,2));
@@ -33,57 +33,36 @@ U = [U,c];
 V = rand(2,d);
 
 totTic=tic;
-epochTimes=zeros(maxIter,1);
 
-for epoch=1:maxIter
-    %if epoch==1 || mod(epoch,10)==0
-        disp(epoch);    
-    %end
-    eTic = tic;
-    
-    %iterate through all sentences
-    for i=1:length(allSNum)
-        %get sentence
-        sent=allSNum(i);
-        sent=sent{1,1};
-        numWords=length(sent);
-        
-        %skip sentences of less than 2 words because our our neural nets
-        %are defined for these
-        if numWords<2
-            continue;
-        end
-        
-        t=labels(i);
-        t=[t; 1-t];
 
-        %build up sentence binary tree, and perform feed forward
-        %   algorithm at the same time
-        [sentTree, outputItr, innerItr, inputItr] = buildTree(sent, meanings, numWords, W, U, V, d, t, alpha);
+i=randi(length(allSNum),1);
+sent=allSNum(i);
+sent=sent{1,1};
+numWords=length(sent);
 
-        %backpropagate
-        [ dW,dU,dV, backTreeZ ] = backProp( sentTree, meanings, t, outputItr, innerItr, inputItr, U, W, V, d, alpha, trainInput );
-        
-        %Numerical Differentiaton
-        E=1e-6;
-        [ numDiffW ] = numDiff( outputItr, innerItr, sentTree, W, U, V, d, t, alpha, E );
-        
-        %Check derivatives
-        D = sum(sum((numDiffW - dW).^2));
+%skip sentences of less than 2 words because our our neural nets
+%are defined for these
+if numWords<2
+    t=labels(i);
+    t=[t; 1-t];
 
-        %Regularized SGD update
-        newW = W - lambda(1)*dW;
-        newU = U - lambda(2)*dU;
-        V = V - lambda(3)*dV;
+    %build up sentence binary tree, and perform feed forward
+    %   algorithm at the same time
+    [sentTree, outputItr, innerItr, inputItr] = buildTree(sent, meanings, numWords, W, U, V, d, t, alpha);
 
-        %Don't regularize intercept
-        W = [newW(:,1:end-1),W(:,end)];
-        U = [newU(:,1:end-1),U(:,end)];
-    end
-    
-    epochTimes(epoch) = toc(eTic);    
+    %backpropagate
+    [ dW,dU,dV, backTreeZ ] = backProp( sentTree, meanings, t, outputItr, innerItr, inputItr, U, W, V, d, alpha, trainInput );
+
+    %Numerical Differentiaton
+    E=1e-6;
+    [ numDiffW ] = numDiff( outputItr, innerItr, sentTree, W, U, V, d, t, alpha, E );
+
+    %Check derivatives
+    D = sum(sum((numDiffW - dW).^2));
+else
+    i=i+1;
 end
 
 totalTime = toc(totTic);
-fprintf('SGD_NN took %f seconds (aka %f minutes).\n\n',totalTime,totalTime/60);
-plot(epochTimes);
+fprintf('Checking one tree took %f seconds (aka %f minutes).\n\n',totalTime,totalTime/60);
+fprintf('The Euclidean distance is %f', D);
