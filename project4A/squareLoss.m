e5cd09f941329c3a0e44f36a8d41ee261dd2310a
Alex Xydes <axydes@gmail.com>
2014-03-16 00:22:36 -0700

    Changing square loss to match paper

--- project4A/squareLoss.m
+++ project4A/squareLoss.m
@@ -2,7 +2,7 @@ function [ err ] = squareLoss( xi, zi, num, denom, alpha )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-    err = alpha * (num/denom) * 0.5*(norm(xi-zi)^2);
+    err = alpha * (num/denom) * 0.5*sum((xi-zi).*(xi-zi));
 
 end
 
