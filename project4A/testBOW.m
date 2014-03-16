ed726b210c87d5d71e60a302f25f7c27faf3a9d4
Alex Xydes <axydes@gmail.com>
2014-03-16 01:40:22 -0700

    Actually added testBOW

---
+++ project4A/testBOW.m
@@ -0,0 +1,51 @@
+function [ pred, totalTime ] = testBOW( data, labels, d, alpha, V, meanings )
+%trainNN build and train the neural network
+
+    numExamples=length(data);
+
+    runPreds = zeros(2,numExamples);
+
+    %Training
+    totTic=tic;
+
+    %iterate through all sentences
+    for i=1:numExamples
+        %get sentence
+        sent=data(i);
+        sent=sent{1,1};
+        numWords=length(sent);
+
+        %skip sentences of less than 2 words because our our neural nets
+        %are defined for these
+        if numWords<2
+            continue;
+        end
+
+        tl=labels(i);
+        t=[tl; 1-tl];
+
+        %Run Bag of Words predictions on each sentence
+        [ BOWlist ] = bagOfWords( sent, meanings, numWords, V, t, alpha );
+
+        %Get average word meaning and predict sentence meaning
+        wordMeaning=zeros(d,numWords);
+        for w=1:numWords
+            word=BOWlist{w,1};
+            wordMeaning(:,w)=word{1};
+        end
+        aveMeaning=mean(wordMeaning,2);
+        p = predictNode(aveMeaning,V);
+        runPreds(:,i)=p;
+    end
+
+    totalTime = toc(totTic);
+    fprintf('SGD_BOW took %f seconds (aka %f minutes) to test.\n\n',totalTime,totalTime/60);
+
+    pred = zeros(numExamples,1);
+    runPreds(2,:)=1-runPreds(1,:);
+    for i=1:numExamples
+         pred(i)=find(runPreds(:,i)>0.5)-1;
+    end
+
+end
+
